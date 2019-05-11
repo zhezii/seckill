10cf93efc4d4fff445c7c9f99a90d381bb779e89
zhezii <1115384864@qq.com>
2019-05-11 14:07:23 +0800

    编写测试用例，修改bug

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -1,5 +1,6 @@
 package org.seckill.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.seckill.entity.Seckill;
 
 import java.util.Date;
@@ -17,7 +18,7 @@ public interface SeckillDao {
      * @param killTime
      * @return 影响的行数
      */
-    int reduceNumber(long seckillId, Date killTime);
+    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
 
     /**
      * 根据id查询秒杀对象
@@ -32,5 +33,5 @@ public interface SeckillDao {
      * @param limit
      * @return
      */
-    List<Seckill> queryAll(int offset,int limit);
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
 }
